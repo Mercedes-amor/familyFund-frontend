52e2742f7ddc139aaa9307f2d3a1734bf9a3cd85
MercedesAmorGallart <127596167+MercedesAmorGallart@users.noreply.github.com>
2025-07-07 10:49:44 +0200

    Conectando Frontend con Backend y pruebas

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import graficImg from "./assets/grafic.svg"
 import Header from "./components/Header";
+import { useEffect, useState } from 'react';
 
 import "./App.css";
 function App() {
+  const [msg, setMsg] = useState('Cargando...');
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/ping')
+      .then(res => res.text())
+      .then(data => setMsg(data))
+      .catch(err => {
+        console.error('Error al conectar con backend:', err);
+        setMsg('Error de conexión');
+      });
+  }, []);
+
   return (
-    <div id="main-container">
+    <div>
+      <h1>Conexión backend</h1>
+      <p>{msg}</p>
       <Header/>
     </div>
   );
 }
-
-//Plantilla de base------
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
 
 export default App;
